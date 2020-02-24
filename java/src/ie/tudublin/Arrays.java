4cfd609f7681a46754be48c56f448b8631b8005e
Kelly-001 <mankieu_01@yahoo.ie>
2020-02-24 12:17:21 +0000

    lol

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,6 +4,13 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet
 {	
+
+
+	//float[] rainfall = new float[12]; //how to make an array
+	float[] rainfall = {45,37,55,27,38,50,79,48,104,31,100,58}; //assigning elements to an array
+	//int to float is okay but other way around is not because a float is bigger than an int
+	String[] months = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+
 	public void settings()
 	{
 		size(500, 500);
@@ -11,10 +18,39 @@ public class Arrays extends PApplet
 
 	public void setup() 
 	{
-		
+		for(int i =0; i < rainfall.length; i++)
+		{
+			println(months[i] + "\t" + rainfall[i]);
+		}
+
+		// for(float f:rainfall)
+		// {
+		// 	println(f);
+		// }
+
+		// for(String s:months)
+		// {
+		// 	println(s);
+		// }
+
+
 	}
 
-	
+	void drawBarChart()
+	{
+		float w =  width / (float) rainfall.length;
+		float cGap = 255 / (float) rainfall.length;
+		noStroke();
+		colorMode(HSB);
+		for(int i=0; i<rainfall.length; i++)
+		{
+			float x = i * w;
+			fill(i * cGap,255,255);
+			rect(x,height,w,-rainfall[i]);
+			
+		}
+
+	}
 
 	float offset = 0;
 
